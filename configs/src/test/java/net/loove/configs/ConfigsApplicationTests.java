9f5281ef918bfee32746ddb35a1b429f3d8d5ca5
goldfinch <dyboshit@gmail.com>
2024-10-27 20:16:07 +0300

    feat: configs service base

---
+++ configs/src/test/java/net/loove/configs/ConfigsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.loove.configs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
