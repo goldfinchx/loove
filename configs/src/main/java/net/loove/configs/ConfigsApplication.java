c74badbf97f0d022c6421038feca012cd2922ff4
goldfinch <dyboshit@gmail.com>
2024-10-27 20:35:54 +0300

    feat: moved configs to configs service

--- configs/src/main/java/net/loove/configs/ConfigsApplication.java
+++ configs/src/main/java/net/loove/configs/ConfigsApplication.java
@@ -2,7 +2,10 @@ package net.loove.configs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigsApplication {
 
