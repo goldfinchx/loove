4f3dd35561a6e8f335216d50b7285c4a8a32db56
goldfinch <dyboshit@gmail.com>
2024-10-28 20:26:59 +0300

    refactor: added status to profile creation method

--- profiles/src/main/java/net/loove/profiles/controller/ProfilesController.java
+++ profiles/src/main/java/net/loove/profiles/controller/ProfilesController.java
@@ -5,11 +5,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.loove.profiles.service.ProfilesService;
 import net.loove.profiles.model.Profile;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,8 +36,9 @@ public class ProfilesController {
     }
 
     @PostMapping
-    public Profile createProfile(@Validated Profile profile) {
-        return this.service.createProfile(profile);
+    public ResponseEntity<Profile> createProfile(@Validated @RequestBody Profile profile) {
+        final Profile createdProfile = this.service.createProfile(profile);
+        return new ResponseEntity<>(createdProfile, HttpStatus.CREATED);
     }
 
 
