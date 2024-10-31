6374c2c9a735a0c4870f31c4a708c75272e55f65
goldfinch <dyboshit@gmail.com>
2024-10-31 13:14:51 +0300

    feat: cache config

---
+++ notifications/src/main/java/net/loove/notifications/configurations/CacheConfiguration.java
@@ -0,0 +1,10 @@
+package net.loove.notifications.configurations;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class CacheConfiguration {
+
+}
